9429ffe8b1ebb5d5c5a9d9538d075e71d3db6af3
sauxpa <patrick.jr.saux@gmail.com>
2020-04-09 13:05:49 +0200

    adapt to new release + add marking modes

--- bond_pricer_app/main.py
+++ bond_pricer_app/main.py
@@ -1,16 +1,20 @@
 import pandas as pd
 from collections import defaultdict
-from bond_pricer import FixedCouponBond
+from bond_pricer import FixedCouponBond, ONE_PCT
 
 from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource, Panel
-from bokeh.models.widgets import TextInput, Tabs, Div
+from bokeh.models.widgets import TextInput, Tabs, Div, Slider
 from bokeh.layouts import layout, WidgetBox
 from bokeh.plotting import figure
 from bokeh.models import NumeralTickFormatter
 
 
-ONE_PCT = 1e-2
+N_MARKING_MODES = 2
+MARKING_MODE_MAP = {
+    1: 'price',
+    2: 'ytm',
+}
 CASHFLOW_WIDTH = 0.05
 
 
@@ -26,10 +30,11 @@ def extract_numeric_input(s: str) -> int:
             raise Exception('{:s} must be numeric.')
 
 
-def make_dataset(coupon,
+def make_dataset(marking_mode_int,
+                 mark,
+                 coupon,
                  maturity,
                  coupon_frequency,
-                 Y,
                  div_,
                  ):
     """Creates a ColumnDataSource object with data to plot.
@@ -44,9 +49,14 @@ def make_dataset(coupon,
         }
     )
 
-    bond = FixedCouponBond(term_sheet)
+    marking_mode = MARKING_MODE_MAP[marking_mode_int]
+    mark *= ONE_PCT
 
-    bond_price = bond.yield_to_price(Y * ONE_PCT)
+    bond = FixedCouponBond(
+        term_sheet,
+        marking_mode,
+        mark,
+        )
 
     df = pd.DataFrame(
         {
@@ -61,7 +71,7 @@ def make_dataset(coupon,
         }
     ).set_index('coupon_dates')
 
-    macaulay_duration = bond.macaulay_duration(bond_price)
+    macaulay_duration = bond.macaulay_duration
     df_duration = pd.DataFrame(
         {
             'x': [macaulay_duration, macaulay_duration],
@@ -69,18 +79,19 @@ def make_dataset(coupon,
         }
     )
 
-    params_text = '<ul>\
-    <li>Price = <b>{:.2f}</b></li>\
-    <li>Duration = {:.2f}</li>\
+    params_text = 'Marking mode: <b>{:s}</b><ul>\
+    <li>Price = {:.2%}</li>\
+    <li>YTM = {:.2%}</li>\
     <li>Macaulay Duration = {:.2f}</li>\
     <li>Modified Duration = {:.2f}</li>\
     <li>DV01 = {:.2f}</li>\
     </ul>'.format(
-        bond_price,
-        bond.duration(bond_price),
+        marking_mode,
+        bond.price,
+        bond.ytm,
         macaulay_duration,
-        bond.modified_duration(bond_price),
-        bond.dv01(bond_price),
+        bond.modified_duration,
+        bond.dv01,
         )
     div_.text = params_text
 
@@ -144,16 +155,18 @@ def update(attr, old, new):
     """Update ColumnDataSource object.
     """
     # Change parameters to selected values
+    marking_mode = marking_mode_select.value
+    mark = extract_numeric_input(mark_select.value)
     coupon = extract_numeric_input(coupon_select.value)
     maturity = extract_numeric_input(maturity_select.value)
     coupon_frequency = extract_numeric_input(coupon_frequency_select.value)
-    Y = extract_numeric_input(yield_select.value)
 
     new_src, new_src_duration = make_dataset(
+        marking_mode,
+        mark,
         coupon,
         maturity,
         coupon_frequency,
-        Y,
         div,
         )
 
@@ -162,33 +175,44 @@ def update(attr, old, new):
     src_duration.data.update(new_src_duration.data)
 
 
-# if __name__ == "__main__":
+# Marking
+marking_mode_select = Slider(start=1,
+                             end=N_MARKING_MODES,
+                             step=1,
+                             title='Marking mode',
+                             value=1,
+                             )
+mark_select = TextInput(value='100.00', title='Mark')
+
+# Bond term sheet
 coupon_select = TextInput(value='5.00', title='Coupon (%)')
 maturity_select = TextInput(value='5', title='Maturity (y)')
 coupon_frequency_select = TextInput(
     value='1',
     title='Coupon Frequency (per year)'
     )
-yield_select = TextInput(value='5.00', title='Yield (%)')
 
 # Update the plot when parameters are changed
+marking_mode_select.on_change('value', update)
+mark_select.on_change('value', update)
 coupon_select.on_change('value', update)
 maturity_select.on_change('value', update)
 coupon_frequency_select.on_change('value', update)
-yield_select.on_change('value', update)
 
+marking_mode = marking_mode_select.value
+mark = extract_numeric_input(mark_select.value)
 coupon = extract_numeric_input(coupon_select.value)
 maturity = extract_numeric_input(maturity_select.value)
 coupon_frequency = extract_numeric_input(coupon_frequency_select.value)
-Y = extract_numeric_input(yield_select.value)
 
 div = Div(text='<b></b><br>', width=250, height=150)
 
 src, src_duration = make_dataset(
+    marking_mode,
+    mark,
     coupon,
     maturity,
     coupon_frequency,
-    Y,
     div,
     )
 
@@ -196,7 +220,8 @@ controls = WidgetBox(
     coupon_select,
     maturity_select,
     coupon_frequency_select,
-    yield_select,
+    marking_mode_select,
+    mark_select,
     div,
     width=250,
     height=550,
